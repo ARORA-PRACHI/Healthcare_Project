fbdbbdcb724310c6cfc8f4e6f0aad47463c5dcb6
Prachi Arora <prachi659.be22@chitkara.edu.in>
2024-10-23 15:27:28 +0530

    HBS

--- server/server.js
+++ server/server.js
@@ -4,7 +4,8 @@ const express=require("express");                              //nodejs framewor
 const connectDb=require("./config/dbConnection");
 const errorHandler=require("./middleware/errorHandler");      // various purposes authorization,error handling,request handling
 const cors=require("cors");                                   // identity verification for security purposes
-
+const hbs = require('hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 
 
 //env file handling
@@ -25,10 +26,21 @@ app.use(errorHandler);
 
 //routes below
 app.get('/',(req,res)=>{
-    res.send("working");
+    res.send("Working");
 })
 
+
+app.get("/home",(req,res)=>{
+    res.render("home",{})
+})
+
+app.get("/users",(req,res)=>{
+    res.render("users",{})
+})
 //app config start
 app.listen(port, ()=>{
     console.log(`Server running on port http://localhost:${port}`);
 });
+
+app.set('view engine','hbs');   // to set Handlebars (hbs) as the templating engine for rendering views.    
+//npm run dev automatic restarts when you make changes to your code.
